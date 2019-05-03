f73ce5254f5ad216610536bae6286ca5f18230f2
blizmanyu <blizman2017@gmail.com>
2019-05-02 22:30:53 -0700

    Initial skeleton design

--- PlaylistCreator/Playlist.cs
+++ PlaylistCreator/Playlist.cs
@@ -8,5 +8,56 @@ namespace PlaylistCreator
 {
 	public class Playlist
 	{
+		public enum PType { English, JPopSpringSummer, JPopFallWinter };
+		public PType Type;
+		public string Name;
+
+		public Playlist(PType type, string name=null)
+		{
+			if (String.IsNullOrWhiteSpace(name))
+				name = type.ToString();
+
+			Type = type;
+			Name = name;
+		}
+
+		public void Create()
+		{
+			switch (Type) {
+				#region JPopSpringSummer
+				case PType.JPopSpringSummer:
+					// Step 1: Get all songs //
+
+					// Step 2: Create "New" list //
+
+					// Step 3: Create "Good" list //
+
+					// Step 4: Create New + Good list //
+
+					// Step 5: Create playlist //
+
+					// Step 6: Write playlist //
+
+					break;
+				#endregion JPopSpringSummer
+				default:
+					break;
+			}
+		}
+
+		public void WriteForBrowser()
+		{
+
+		}
+
+		public void WriteForITunes()
+		{
+
+		}
+
+		public void WriteForWinamp()
+		{
+
+		}
 	}
 }
