88a01f33ba8f4a1598ad7d176e893cff461984e7
blizmanyu <blizman2017@gmail.com>
2019-05-03 19:21:29 -0700

    WIP: 2019 0503 1921

--- PlaylistCreator/Playlist.cs
+++ PlaylistCreator/Playlist.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yutaka.IO;
 
 namespace PlaylistCreator
 {
@@ -20,7 +21,12 @@ namespace PlaylistCreator
 		public List<SongFileInfo> GoodJPopFallWinterSongs = new List<SongFileInfo> {
 			new SongFileInfo("artist", "title"),
 		};
+		public DateTime NewSongThreshold;
+		public string SourceFolder; // where the music files are //
+		public string DestinationFolder; // where you want the playlist to go //
 
+		protected FileUtil _fileUtil;
+		protected HashSet<string> FolderExclusions;
 		protected List<SongFileInfo> AllSongs = new List<SongFileInfo>();
 		protected List<SongFileInfo> GoodList = new List<SongFileInfo>();
 		protected List<SongFileInfo> NewList = new List<SongFileInfo>();
@@ -34,6 +40,10 @@ namespace PlaylistCreator
 
 			Type = type;
 			Name = name;
+			NewSongThreshold = DateTime.Now.AddYears(-2);
+			SourceFolder = @"C:\Music\00 Genres\";
+			DestinationFolder = @"C:\Music\01 Playlists\";
+			_fileUtil = new FileUtil();
 
 			if (createPlaylistNow)
 				Create();
@@ -41,10 +51,33 @@ namespace PlaylistCreator
 
 		public void Create()
 		{
+			List<string> files;
 			switch (Type) {
+				#region English
+				case PType.English:
+					FolderExclusions = new HashSet<string>() { @"\Album", @"\Classical", @"\J-Pop", @"\J-Rap", @"\Spanish", };
+
+					// Step 1: Get all songs //
+					files = _fileUtil.GetAllAudioFiles(SourceFolder, FolderExclusions.ToArray());
+					for (int i = 0; i < files.Count; i++)
+						AllSongs.Add(new SongFileInfo(files[i]));
+
+					// Step 2: Create GoodList //
+
+					// Step 3: Create NewList //
+
+					// Step 4: Create NewPlusGoodList //
+
+					// Step 5: Create ThePlaylist //
+
+					break;
+				#endregion English
 				#region JPopSpringSummer
 				case PType.JPopSpringSummer:
 					// Step 1: Get all songs //
+					files = _fileUtil.GetAllAudioFiles(SourceFolder, FolderExclusions.ToArray());
+					for (int i = 0; i < files.Count; i++)
+						AllSongs.Add(new SongFileInfo(files[i]));
 
 					// Step 2: Create GoodList //
 
